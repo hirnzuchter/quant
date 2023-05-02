4c63d335f09d0a44e9b475257a37045d816955d4
hirnzuchter <103340555+hirnzuchter@users.noreply.github.com>
2023-05-01 19:03:50 -0500

    first commit

---
+++ ftfta.py
@@ -0,0 +1,23 @@
+import requests
+import datetime
+from scipy.fft import fft, ifft
+import numpy as np
+import matplotlib.pyplot as plt
+
+def denoise(symbol, days):
+    date = datetime.date.today()
+    url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/minute/{date - datetime.timedelta(days=days+2)}/{date}?adjusted=true&sort=asc&limit=120&apiKey=boxpAGWE_Xk0mYgLO4zX72bVQIJVSxwL'
+    data = requests.get(url).json()['results']
+    data = np.array([x['c'] for x in data])
+    plt.plot(data)
+    plt.title(f"{symbol} Price Data")
+    plt.show()
+    ft = fft(data)
+    q = np.quantile(ft, 0.9)
+    ft = ft[ft > q]
+    data = ifft(ft)
+    plt.plot(data)
+    plt.title("Denoised Trend")
+    plt.show()
+
+denoise(symbol="GOOG",days=30)
